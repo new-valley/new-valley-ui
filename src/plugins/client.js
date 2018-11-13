799be30cc3731bd9632afa8aa447d1cf2013125b
Luiz Castro <fellipeluizz@gmail.com>
2018-11-13 20:07:27 -0200

    create client get functions

--- src/plugins/client.js
+++ src/plugins/client.js
@@ -3,15 +3,17 @@ import axios from 'axios'
 export default function (Vue, options) {
     axios.defaults.baseURL = 'http://localhost:5000/api'
     Vue.Client = {
-        async get (uri, id) {
+        async get (uri, id, resource) {
             if(id) {
                 uri = `${uri}/${id}`
+                if(resource)
+                    uri = `${uri}/${resource}`
             }
             const response = await axios.get(uri)
             return response.data.data
         },
-        async getSubForums (id) {
-            return await this.get('/subforums', id)
+        async getSubForums (id, resource) {
+            return await this.get('/subforums', id, resource)
         },
         async getAvatars (id) {
             return await this.get('/avatars', id)
@@ -19,11 +21,11 @@ export default function (Vue, options) {
         async getPosts (id) {
             return await this.get('/posts', id)
         },
-        async getUsers (id) {
-            return await this.get('/users', id)
+        async getUsers (id, resource) {
+            return await this.get('/users', id, resource)
         },
-        async getTopcs (id) {
-            return await this.get('/topics', id)
+        async getTopcs (id, resource) {
+            return await this.get('/topics', id, resource)
         },
     }
 
