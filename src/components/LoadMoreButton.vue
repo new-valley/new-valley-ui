53088cc248db33c0a3e884f666e1745f7586edb6
Erik Perillo <erik.perillo@gmail.com>
2018-11-18 01:38:02 -0200

    added LoadMoreButton component and added it to topic view

---
+++ src/components/LoadMoreButton.vue
@@ -0,0 +1,19 @@
+<template>
+  <v-layout row justify-center>
+    <v-btn dark @click="onClick">Carregar mais</v-btn>
+  </v-layout>
+</template>
+
+<script>
+  export default {
+    props: {
+      onClick: {
+        type: Function,
+        required: true,
+      }
+    },
+    data() {
+      return {}
+    }
+  }
+</script>
