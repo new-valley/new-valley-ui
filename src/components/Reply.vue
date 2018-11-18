ee310be50b6afba32ba3c8275a80b8c4faa1120e
Erik Perillo <erik.perillo@gmail.com>
2018-11-18 01:38:46 -0200

    added basic reply component

---
+++ src/components/Reply.vue
@@ -0,0 +1,34 @@
+<template>
+  <v-layout row justify-center>
+    <v-dialog v-model="dialog" persistent max-width="600px">
+      <v-btn slot="activator" dark>Responder</v-btn>
+      <v-card>
+        <v-card-title>
+          <span class="headline">Responder</span>
+        </v-card-title>
+        <v-card-text>
+          <v-container grid-list-md>
+            <v-layout wrap>
+              <v-flex xs12>
+                <v-text-field label="Texto" required></v-text-field>
+              </v-flex>
+            </v-layout>
+          </v-container>
+        </v-card-text>
+        <v-card-actions>
+          <v-spacer></v-spacer>
+          <v-btn color="blue darken-1" flat @click.native="dialog = false">Enviar</v-btn>
+          <v-btn color="blue darken-1" flat @click.native="dialog = false">Cancelar</v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-dialog>
+  </v-layout>
+</template>
+
+<script>
+  export default {
+    data: () => ({
+      dialog: false
+    })
+  }
+</script>
