9d3abe19e9693ce725020528f32d0733295b0910
Erik Perillo <erik.perillo@gmail.com>
2018-11-18 02:00:43 -0200

    added basic reply and load-more-button to topic view

--- src/views/Topic.vue
+++ src/views/Topic.vue
@@ -5,14 +5,32 @@
         <basic-list :items="items"/>
       </v-flex>
     </v-layout>
+    <v-layout row>
+      <v-flex xs12 sm6 offset-sm3>
+        <v-card style="padding: 10px;">
+          <reply/>
+        </v-card>
+      </v-flex>
+    </v-layout>
+    <v-layout row>
+      <v-flex xs12 sm6 offset-sm3>
+        <v-card style="padding: 10px;">
+          <load-more-button :onClick="fetchPostsBlock"/>
+        </v-card>
+      </v-flex>
+    </v-layout>
   </div>
 </template>
 
 <script>
   import BasicList from '../components/BasicListPage'
+  import Reply from '../components/Reply'
+  import LoadMoreButton from '../components/LoadMoreButton'
   export default {
     components: {
-      BasicList
+      BasicList,
+      Reply,
+      LoadMoreButton,
     },
     props: {
       id: {
@@ -24,7 +42,31 @@
       return {
         items: [
             { header: 'topic' }
-        ]
+        ],
+        fetchOffset: 0,
+        fetchNumPosts: 4,
+      }
+    },
+    methods: {
+      async fetchPostsBlock() {
+        if(this.fetchOffset || this.fetchOffset === 0) {
+          const posts = await this.$client.getTopics(
+            this.id, 'posts',
+            `order=oldest&max_n_results=${this.fetchNumPosts}&offset=${this.fetchOffset}`)
+          posts.map((post) => {
+            const item = {
+              avatar: post.user.avatar.uri,
+              title: post.user.username,
+              subtitle: post.content,
+              to: `/p/${post.post_id}`
+            }
+            const divider = { divider: true, inset: true }
+            this.items.push(item, divider)
+          })
+          if(posts.length === 0) {
+            this.fetchOffset = null
+          }
+        }
       }
     },
     async created () {
@@ -32,17 +74,7 @@
         .then((topic) => {
             this.items[0] = { header: topic.title }
         })
-      const posts = await this.$client.getTopics(this.id, 'posts')
-      posts.map((post) => {
-        const item = {
-          avatar: post.user.avatar.uri,
-          title: post.user.username,
-          subtitle: post.content,
-          to: `/p/${post.post_id}`
-        }
-        const divider = { divider: true, inset: true }
-        this.items.push(item, divider)
-      })
+      this.fetchPostsBlock()
     }
   }
 </script>
