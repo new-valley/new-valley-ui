ae809a426617031dc91fc254fe99d99af5aa6b8c
Erik Perillo <erik.perillo@gmail.com>
2018-11-24 14:48:45 -0200

    update on list items (subforum/topic)

--- src/components/TopicListItem.vue
+++ src/components/TopicListItem.vue
@@ -3,20 +3,15 @@
   <v-layout row wrap justify-center>
      <v-flex xs12>
        <v-card dark color="blue-grey darken-2" class="white--text">
-         <router-link :to="to">
-           <div style="padding: 16px 8px 16px 20px;">
-             <h2 class="headline" style="overflow-wrap: break-word;">{{ title }}</h2>
-           </div>
-         </router-link>
+          <v-card-title style="padding-top: 20px;" primary-title>
+            <div>
+              <router-link :to="to">
+                <div class="headline">{{ title }}</div>
+              </router-link>
+              <span>by <b>{{ author }}</b></span>
+            </div>
+          </v-card-title>
          <v-layout row wrap justify-center style="padding: 4px 20px 20px 16px;">
-           <v-flex xs4>
-             <v-layout row wrap justify-center align-center fill-height>
-               <v-flex xs2><p style="margin: 0;" class="text-xs-right"><v-icon>account_box</v-icon></p></v-flex>
-               <v-flex xs10><p style="margin: 0 0 0 8px; font-size: 100%; overflow: hidden;">
-                 {{ author }}
-               </p></v-flex>
-             </v-layout>
-           </v-flex>
            <v-flex xs3>
              <v-layout row wrap justify-center align-center fill-height>
                <v-flex xs2><p style="margin: 0;" class="text-xs-right"><v-icon>mail</v-icon></p></v-flex>
@@ -27,14 +22,22 @@
            </v-flex>
            <v-flex xs5>
              <v-layout row wrap justify-center align-center fill-height>
-               <v-flex xs2><p style="margin: 0;" class="text-xs-right"><v-icon>calendar_today</v-icon></p></v-flex>
+               <v-flex xs2><p style="margin: 0;" class="text-xs-right"><v-icon>today</v-icon></p></v-flex>
                <v-flex xs10><p style="margin: 0 0 0 8px; overflow: hidden;">
                  {{ lastPostDatetime }}
                </p></v-flex>
              </v-layout>
            </v-flex>
-         </v-layout>
-         <v-divider dark v-if="divider"/>
+           <v-flex xs4>
+             <v-layout row wrap justify-center align-center fill-height>
+               <v-flex xs2><p style="margin: 0;" class="text-xs-right"><v-icon>directions_walk</v-icon></p></v-flex>
+               <v-flex xs10><p style="margin: 0 0 0 8px; font-size: 100%; overflow: hidden;">
+                 {{ lastAuthor }}
+               </p></v-flex>
+             </v-layout>
+           </v-flex>
+          </v-layout>
+       <v-divider dark v-if="divider"/>
       </v-card>
     </v-flex>
   </v-layout>
@@ -63,6 +66,11 @@
         type: String,
         required: false,
         default: '1984-01-02'
+      },
+      lastAuthor: {
+        type: String,
+        required: false,
+        default: 'user'
       },
       to: {
         type: String,
